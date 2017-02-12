34e0bfa515b7f991dcc362b0c190771841deb507
amroshehk <amroshehzen@hotmail.com>
2017-02-12 03:04:45 +0200

    add help and login and setting and task Activities

---
+++ app/src/main/java/com/applefish/gheraspoint/fcm/EndPoints.java
@@ -0,0 +1,13 @@
+package com.applefish.gheraspoint.fcm;
+
+/**
+ * Created by Amro on 13/01/2017.
+ */
+
+public class EndPoints {
+    public static final String URL_REGISTER_DEVICE = "http://gherasbirr.org/gheraspoints/fcm/RegisterDevice.php";
+    public static final String URL_UNREGISTER_DEVICE = "http://gherasbirr.org/gheraspoints/fcm/unRegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "http://gherasbirr.org/gheraspoints/fcm/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "http://gherasbirr.org/gheraspoints/fcm/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "http://gherasbirr.org/gheraspoints/fcm/GetRegisteredDevices.php";
+}
---
+++ app/src/main/java/com/applefish/gheraspoint/fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,35 @@
+package com.applefish.gheraspoint.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Amro on 12/01/2017.
+ */
+
+//Class extending FirebaseInstanceIdService
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        //calling the method store token and passing token
+        storeToken(refreshedToken);
+    }
+
+    private void storeToken(String token) {
+        //saving the token on shared preferences
+        SharedPrefManager.getInstance(getApplicationContext()).saveDeviceToken(token);
+
+    }
+}
